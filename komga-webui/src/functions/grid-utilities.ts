785c6a043b09f9e5f6bf447641941519a4dc3467
dandelions <394854846@qq.com>
2026-06-08 16:05:30 +0800

    Tighten narrow item card layout

--- komga-webui/src/functions/grid-utilities.ts
+++ komga-webui/src/functions/grid-utilities.ts
@@ -1,15 +1,12 @@
 export function computeCardWidth (width: number, breakpoint: string, cardPadding: number = 16): number {
   const defaultWidth = 150
-  const twoColumnsMinWidth = (defaultWidth + cardPadding) * 2
+  const compactWidth = 120
+  const compactContainerMaxWidth = (defaultWidth + cardPadding) * 3
+  const layoutSafety = 8
 
-  if (width < twoColumnsMinWidth) {
-    return (width - (cardPadding * 2)) / 2
+  if (breakpoint === 'xs' || width < compactContainerMaxWidth) {
+    return Math.min(compactWidth, (width - (cardPadding * 2) - layoutSafety) / 2)
   }
 
-  switch (breakpoint) {
-    case 'xs':
-      return (width - (cardPadding * 2)) / 2
-    default:
-      return defaultWidth
-  }
+  return defaultWidth
 }
