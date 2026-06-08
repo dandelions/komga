f8cf14acddb94f73b21a953541abf54865c41608
dandelions <394854846@qq.com>
2026-06-08 15:14:55 +0800

    Keep narrow item grids at two columns

--- komga-webui/src/functions/grid-utilities.ts
+++ komga-webui/src/functions/grid-utilities.ts
@@ -1,8 +1,15 @@
 export function computeCardWidth (width: number, breakpoint: string, cardPadding: number = 16): number {
+  const defaultWidth = 150
+  const twoColumnsMinWidth = (defaultWidth + cardPadding) * 2
+
+  if (width < twoColumnsMinWidth) {
+    return (width - (cardPadding * 2)) / 2
+  }
+
   switch (breakpoint) {
     case 'xs':
       return (width - (cardPadding * 2)) / 2
     default:
-      return 150
+      return defaultWidth
   }
 }
